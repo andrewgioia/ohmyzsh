57d49bd4003f4f40c948db3125714f73b7874212
Andrew Gioia <andrew@gioia.email>
2020-08-19 09:56:23 -0400

    Removes vagrant plugin

--- custom/plugins/homestead-vagrant/homestead-vagrant.plugin.zsh
+++
@@ -1,5 +0,0 @@
-# allow `homestead up` to run `vagrant up` within the Homestead directory
-# from anywhere; you can pass any flag or command though
-function homestead() {
-    ( cd ~/Projects/Homestead && vagrant $* )
-}
